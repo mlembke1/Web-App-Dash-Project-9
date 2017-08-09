964c8a00931d2d8519aeb9b75fa4599c969641e0
Mitch Lembke <mitchell.lembke@gmail.com>
2017-08-10 08:18:40 +0900

    Commit 5
    Added the first chart! Woo hoo!  A line chart! :)

--- js/main.js
+++ js/main.js
@@ -3,39 +3,47 @@
 
 swal({
   title: "Welcome!",
-  text: "Feel free to explore all of this weather data for Mt. Fuji !",
+  text: "Let's see how much traffic YourApp has!",
   type: "info",
   confirmButtonText: "Explore!",
 });
 
 
-
+const days =   ["Sunday", "Monday", "Tuesday", "Wednesday",
+                        "Thursday", "Friday", "Saturday"];
+const weeks =   ["Week 1", "Week 2", "Week 3", "Week 4", "Week5", "Week 6", "Week 7"]
 var ctx = document.getElementById("line-chart");
+
+// Chart.defaults.global.responsive = false;
+
 var myChart = new Chart(ctx, {
-    type: 'bar',
+    type: 'line',
     data: {
-        labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
-        datasets: [{
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)'
-            ],
-            borderColor: [
-                'rgba(255,99,132,1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)'
-            ],
-            borderWidth: 1
-        }]
+        labels: days,
+        datasets: [
+          {
+            label: 'Daily Users',
+            lineTension: 0.2,
+            data: [12, 19, 3, 5, 2, 3, 11],
+            backgroundColor: 'rgba(167,219,216, 0.2)',
+            borderColor: 'rgba(243,134,48,1)' ,
+            borderWidth: 1,
+            pointBackgroundColor: 'rgb(105,210,231)',
+            pointBorderColor: 'rgb(105,210,231)',
+            pointRadius: 2,
+            pointHoverBackgroundColor: 'rgb(224,228,204)',
+            pointHoverRadius: 10
+          },
+
+        // {
+        //     label: 'Monthly Users',
+        //     lineTension: 0.2,
+        //     data: [133, 84, 21, 46, 25, 56, 140],
+        //     backgroundColor: 'rgba(167,0,216, 0.2)',
+        //     borderColor: 'rgba(24,134,5,1)',
+        //     borderWidth: 1
+        // }
+      ]
     },
     options: {
         scales: {
