9002895da1742574b6a7bc4ed48c67c08a1366e9
Mitch Lembke <mitchell.lembke@gmail.com>
2017-08-12 19:36:21 +0900

    Commit 7 - Nav and Line Chart changes
    Switching up the nav, working with the general layout, and most of all,
    trying to make the line chart interactive with days, weeks, and months
    data.

--- js/main.js
+++ js/main.js
@@ -11,21 +11,24 @@ swal({
 
 const days =   ["Sunday", "Monday", "Tuesday", "Wednesday",
                         "Thursday", "Friday", "Saturday"];
-const weeks =   ["Week 1", "Week 2", "Week 3", "Week 4", "Week5", "Week 6", "Week 7"]
-var ctx = document.getElementById("line-chart");
 
-// Chart.defaults.global.responsive = false;
+const weeks =   ["Week 1", "Week 2", "Week 3", "Week 4", "Week5", "Week 6", "Week 7"];
 
-var myChart = new Chart(ctx, {
+const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+var ctx= document.getElementById("line-chart");
+
+
+var lineChart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: days,
+        labels:  days,
         datasets: [
           {
-            label: 'Daily Users',
+            label: 'Daily',
             lineTension: 0.2,
             data: [12, 19, 3, 5, 2, 3, 11],
-            backgroundColor: 'rgba(167,219,216, 0.2)',
+            backgroundColor: 'rgba(153,103,55, 1)',
             borderColor: 'rgba(243,134,48,1)' ,
             borderWidth: 1,
             pointBackgroundColor: 'rgb(105,210,231)',
@@ -35,14 +38,32 @@ var myChart = new Chart(ctx, {
             pointHoverRadius: 10
           },
 
-        // {
-        //     label: 'Monthly Users',
-        //     lineTension: 0.2,
-        //     data: [133, 84, 21, 46, 25, 56, 140],
-        //     backgroundColor: 'rgba(167,0,216, 0.2)',
-        //     borderColor: 'rgba(24,134,5,1)',
-        //     borderWidth: 1
-        // }
+        {
+            label: 'Weekly',
+            lineTension: 0.2,
+            data: [133, 84, 21, 46, 25, 56, 140],
+            backgroundColor: 'rgba(250,105, 0, 1)',
+            borderColor: 'rgba(167,219,216, 1)',
+            borderWidth: 1,
+            pointBackgroundColor: 'rgb(105,210,231)',
+            pointBorderColor: 'rgb(105,210,231)',
+            pointRadius: 2,
+            pointHoverBackgroundColor: 'rgb(224,228,204)',
+            pointHoverRadius: 10
+        },
+        {
+            label: 'Monthly',
+            lineTension: 0.2,
+            data: [333, 204, 51, 146, 100, 166, 360],
+            backgroundColor: 'rgba(167,219,216, 1)',
+            borderColor: 'rgba(167,219,216, 1)',
+            borderWidth: 1,
+            pointBackgroundColor: 'rgb(105,210,231)',
+            pointBorderColor: 'rgb(105,210,231)',
+            pointRadius: 2,
+            pointHoverBackgroundColor: 'rgb(224,228,204)',
+            pointHoverRadius: 10
+        }
       ]
     },
     options: {
@@ -55,3 +76,16 @@ var myChart = new Chart(ctx, {
         }
     }
 });
+
+
+// Input element aligned to the right when the screen is at least 768px wide
+// let width = $(window).width();
+//
+// const searchBar = $("#search");
+//
+// if ( width >= "768px") {
+//   searchBar.attr("align","right");
+// }
+// else {
+//   searchBar.removeAttr("align");
+// }
