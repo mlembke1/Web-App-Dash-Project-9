cfffaad7dc8ba21fe48fad80d68727db431aaf72
Mitch Lembke <mitchell.lembke@gmail.com>
2017-08-17 15:38:28 +0900

    Commit 10 - added hourly line chart
    Added hourly line chart

--- js/main.js
+++ js/main.js
@@ -14,7 +14,7 @@ swal({
 
 // *********************************LINE CHART! ***************************************
 
-
+const hours = ['6 a.m.', '8 a.m.', '10 a.m.', '12 p.m.', '2 p.m.', '4 p.m.', '6 p.m.'];
 
 const days =   ["Sunday", "Monday", "Tuesday", "Wednesday",
                         "Thursday", "Friday", "Saturday"];
@@ -22,6 +22,48 @@ const days =   ["Sunday", "Monday", "Tuesday", "Wednesday",
 const weeks =   ["Week 1", "Week 2", "Week 3", "Week 4", "Week5", "Week 6", "Week 7"];
 
 const months = ["January", "February", "March", "April", "May", "June", "July"];
+
+
+
+
+// *************************************** Hourly Line Chart ******************************************
+
+Chart.defaults.global.legend.display = false;
+
+var ctx= document.getElementById("hourly-line-chart");
+
+
+var lineChart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels:  hours,
+        datasets: [
+          {
+            label: 'Hours',
+            lineTension: 0.2,
+            data: [1, 3, 3, 5, 7, 4, 11],
+            backgroundColor: 'rgba(105,210,231, .7)',
+            borderColor: 'rgba(243,134,48,1)' ,
+            borderWidth: 1,
+            pointBackgroundColor: 'rgba(250,105, 0, .5)',
+            pointBorderColor: 'rgba(250,105, 0, .5)',
+            pointRadius: 2,
+            pointHoverBackgroundColor: 'rgb(224,228,204)',
+            pointHoverRadius: 10
+          }
+      ]
+    },
+    options: {
+        scales: {
+            yAxes: [{
+                ticks: {
+                    beginAtZero:true
+                }
+            }]
+        }
+    }
+});
+
 
 
 
@@ -139,15 +181,28 @@ var lineChart = new Chart(ctx, {
 
 // Here we are just making the line chart navigation interactive
 
+// Hourly button
+$('#nav-hourly').click(function() {
+  $('#hourly-line-chart').show();
+  $('#weekly-line-chart').hide();
+  $('#monthly-line-chart').hide();
+  $('#daily-line-chart').hide();
+  $('#nav-hourly').css('background-color', 'rgba(243,134,48,.5)');
+  $('#nav-daily').css('background-color', 'rgb(224,228,204)');
+  $('#nav-weekly').css('background-color', 'rgb(224,228,204)');
+  $('#nav-monthly').css('background-color', 'rgb(224,228,204)');
+});
+
 // Daily button
 $('#nav-daily').click(function() {
   $('#daily-line-chart').show();
   $('#weekly-line-chart').hide();
   $('#monthly-line-chart').hide();
+  $('#hourly-line-chart').hide();
   $('#nav-daily').css('background-color', 'rgba(243,134,48,.5)');
   $('#nav-weekly').css('background-color', 'rgb(224,228,204)');
   $('#nav-monthly').css('background-color', 'rgb(224,228,204)');
-
+  $('#nav-hourly').css('background-color', 'rgb(224,228,204)');
 });
 
 
@@ -156,9 +211,11 @@ $('#nav-weekly').click(function() {
   $('#weekly-line-chart').show();
   $('#daily-line-chart').hide();
   $('#monthly-line-chart').hide();
+  $('#hourly-line-chart').hide();
   $('#nav-weekly').css('background-color', 'rgba(243,134,48,.5)');
   $('#nav-daily').css('background-color', 'rgb(224,228,204)');
   $('#nav-monthly').css('background-color', 'rgb(224,228,204)');
+  $('#nav-hourly').css('background-color', 'rgb(224,228,204)');
 });
 
 
@@ -167,18 +224,22 @@ $('#nav-monthly').click(function() {
   $('#monthly-line-chart').show();
   $('#weekly-line-chart').hide();
   $('#daily-line-chart').hide();
+  $('#hourly-line-chart').hide();
   $('#nav-monthly').css('background-color', 'rgba(243,134,48,.5)');
   $('#nav-daily').css('background-color', 'rgb(224,228,204)');
   $('#nav-weekly').css('background-color', 'rgb(224,228,204)');
+  $('#nav-hourly').css('background-color', 'rgb(224,228,204)'); 
 });
 
 // Here we set what will be visible(concerning the first line chart)
 // when the user first comes to the page/app
+$('#daily-line-chart').hide();
 $('#weekly-line-chart').hide();
 $('#monthly-line-chart').hide();
 
+
 //  Setting the daily button in the line chart nav as the selected button by default
-$('#nav-daily').css('background-color', 'rgba(243,134,48,.5)');
+$('#nav-hourly').css('background-color', 'rgba(243,134,48,.5)');
 $('.line-chart-nav ul li:hover').css('background-color', 'rgba(243,134,48,.5)');
 
 
@@ -240,6 +301,9 @@ var lineChart = new Chart(ctx, {
       ]
     },
     options: {
+      legend: {
+        display: true
+      },
       cutoutPercentage: 70,
       animation: {
         animateScale: true
